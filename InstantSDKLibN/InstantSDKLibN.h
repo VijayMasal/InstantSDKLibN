96d1c16d032a5891e115e91bdc6f96cffdf85013
vijay <vdmasal@gmail.com>
2017-12-14 16:21:02 +0530

    Initial Commit

---
+++ InstantSDKLibN/InstantSDKLibN.h
@@ -0,0 +1,19 @@
+//
+//  InstantSDKLibN.h
+//  InstantSDKLibN
+//
+//  Created by Emberify_Vijay on 14/12/17.
+//  Copyright © 2017 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InstantSDKLibN.
+FOUNDATION_EXPORT double InstantSDKLibNVersionNumber;
+
+//! Project version string for InstantSDKLibN.
+FOUNDATION_EXPORT const unsigned char InstantSDKLibNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstantSDKLibN/PublicHeader.h>
+
+
